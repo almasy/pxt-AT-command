b3fd336b30abebfeaa97d30ac3584137a3c90f2b
Lionkk <Lion_yan@outlook.com>
2019-12-19 10:46:18 +0800

    更新。

--- main.ts
+++ main.ts
@@ -1,7 +1,7 @@
 //% weight=100 color=#0fbc11 icon="\uf1eb" block="WIFI_AT"
 namespace WIFI_AT {
     //% block="Send AT command %s to WIFI module"
-    export function foo(s: string): void {
+    export function sendAT(s: string): void {
         serial.writeString(s + "\u000D" + "\u000A")
     }
 

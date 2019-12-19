b3b9e804b0fae4d3a6a3df3f5b9644bc37633948
Lionkk <Lion_yan@outlook.com>
2019-12-19 10:45:31 +0800

    更新。

--- main.ts
+++ main.ts
@@ -1,16 +1,5 @@
-
-/**
- * 使用此文件来定义自定义函数和图形块。
- * 想了解更详细的信息，请前往 https://makecode.microbit.org/blocks/custom
- */
-
-
-//% weight=100 color=#0fbc11 icon="" block="ATcommand"
-namespace ATcommand {
-    /**
-     * TODO: 在此处描述您的函数
-     * @param s 在此处描述参数, eg: "Hello"
-     */
+//% weight=100 color=#0fbc11 icon="\uf1eb" block="ATcommand"
+namespace WIFI_AT {
     //% block="Send AT command %s to WIFI module"
     export function foo(s: string): void {
         serial.writeString(s + "\u000D" + "\u000A")

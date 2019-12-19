39b6ba3277027c593c6a61e94fa896bc3f9d75d6
Lionkk <Lion_yan@outlook.com>
2019-12-19 10:54:22 +0800

    更新。

--- main.ts
+++ main.ts
@@ -1,8 +1,18 @@
-//% weight=100 color=#0fbc11 icon="\uf1eb" block="WIFI_AT"
-namespace WIFI_AT {
-    //% block="Send AT command %s to WIFI module"
+//% weight=100 color=#0fbc11 icon="" block="AT"
+namespace AT {
+    //% block="Send AT command %s to serial"
     export function sendAT(s: string): void {
         serial.writeString(s + "\u000D" + "\u000A")
     }
+    //% block="set UART |RX %tx|TX %rx|Baud rate %baudrate"
+    //% tx.defl=SerialPin.P8
+    //% rx.defl=SerialPin.P12
+    export function initWIFI(tx: SerialPin, rx: SerialPin, baudrate: BaudRate) {
+        serial.redirect(
+            tx,
+            rx,
+            baudrate
+        )
 
+}
 }

6e4f3f5e074399c262cb2fc2fedfe6f6c0fa314b
Lionkk <Lion_yan@outlook.com>
2019-12-19 10:41:57 +0800

    更新。

--- main.ts
+++ main.ts
@@ -1,7 +1,19 @@
-basic.showLeds(`
-    . . . . .
-    . # . # .
-    . . . . .
-    # . . . #
-    . # # # .
-    `);
+
+/**
+ * 使用此文件来定义自定义函数和图形块。
+ * 想了解更详细的信息，请前往 https://makecode.microbit.org/blocks/custom
+ */
+
+
+//% weight=100 color=#0fbc11 icon=""
+namespace custom {
+    /**
+     * TODO: 在此处描述您的函数
+     * @param s 在此处描述参数, eg: "Hello"
+     */
+    //% block="Send AT command %s to WIFI module"
+    export function foo(s: string): void {
+        serial.writeString(s + "\u000D" + "\u000A")
+    }
+
+}
